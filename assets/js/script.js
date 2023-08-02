1f692f761c8c92bfecf696a73afb250acdaff689
pgpaus <>
2023-08-02 08:43:05 +0000

    Display addition question to the user

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,22 +10,31 @@ document.addEventListener("DOMContentLoaded", function() {
                 alert("You clicked submit!");
             } else {
                 let gameType = this.getAttribute("data-type");
-                alert(`You clicked €{gameType`)
+                runGame(gameType);
             }
         })
     }
+
+    runGame("addition");
 })
     
 /**
  * The main game "loop", called when the script is first loaded
  * and after the user's answer has been processed
  */
-function runGame() {
+function runGame(gameType) {
 
     // Creates two random numbers between 1 through 25
 
     let num1 = Math.floor(Math.random() * 25);
     let num2 = Math.floor(Math.random() * 25);
+
+    if (gameType === "addition") {
+        displayAdditionQuestion(num1, num2);
+    } else {
+        alert(`Unkown game type: ${gameType}`);
+        throw `unkown game type: ${gameType}. Aborting!`;
+    }
 }
 
 
@@ -45,7 +54,11 @@ function incrementWrongAnswer() {
 
 }
 
-function displayAdditionQuestion() {
+function displayAdditionQuestion(opperand1, opperand2) {
+    document.getElementById('opperand1').textContent = opperand1;
+    document.getElementById('opperand2').textContent = opperand2;
+    document.getElementById('operator').textContent = "+";
+
 
 }
 
